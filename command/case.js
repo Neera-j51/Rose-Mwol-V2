910a0e4db8bc5d3f70c26d93bf3b513e8f58f345
Sachu <93819264+Sachu-Settan@users.noreply.github.com>
2022-03-17 15:01:51 +0530

    Update case.js

--- command/case.js
+++ command/case.js
@@ -708,7 +708,15 @@ if (!isrose) return sendButMessage(from, lang.noregis(pushname), `Click Button T
     rose.sendMessage(from, po, image, { quoted: mek, caption: "Selesai" })
     }
     break
-
+//maker textpro
+case 'blackpink':case 'neon':case 'greenneon':case 'advanceglow':case 'futureneon':case 'sandwriting':case 'sandsummer':case 'sandengraved':case 'metaldark':case 'neonlight':case 'holographic':case 'text1917':case 'minion':case 'deluxesilver':case 'newyearcard':case 'bloodfrosted':case 'halloween':case 'jokerlogo':case 'fireworksparkle':case 'natureleaves':case 'bokeh':case 'toxic':case 'strawberry':case 'box3d':case 'roadwarning':case 'breakwall':case 'icecold':case 'luxury':case 'cloud':case 'summersand':case 'horrorblood':case 'thunder':{
+if (!isrose) return sendButMessage(from, lang.noregis(pushname), `Klik Button Untuk Verify`, [{buttonId: '.daftar',buttonText: {displayText: `Daftar`,},type: 1,}], {quoted: fgif});
+    if (args.length == 0) return reply(`Example: ${prefix + command} rose`)
+    ini_txt = args.join(" ")
+    let gambar = await getBuffer(`https://api.lolhuman.xyz/api/textprome/${command}?apikey=${lolkey}&text=${ini_txt}`)
+    rose.sendMessage(from, gambar, image, { quoted: mek, caption: "Sukses" })
+    }
+    break
 //group
 case 'register': case 'reg': case 'register': case 'verify': case 'verif':
 			if (isrose) return  reply(lang.regis())
